2ca77d9e06530d7567b3ee8e255b7d8e2ed32766
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-08-23 13:08:22 -0300

    traduzindo algumas partes

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -23,11 +23,11 @@ export default async function CardWrapper() {
   } = await fetchCardData();
   return (
     <>
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Coletada" value={totalPaidInvoices} type="collected" />
+      <Card title="Pendentes" value={totalPendingInvoices} type="pending" />
+      <Card title="Total faturas" value={numberOfInvoices} type="invoices" />
       <Card
-        title="Total Customers"
+        title="Total clientes"
         value={numberOfCustomers}
         type="customers"
       />
